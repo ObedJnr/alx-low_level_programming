20f6b6b021319ba8031fba0ea5fb4d91fbb936bd
Obed Babington <obedalloteyb@gmail.com>
2023-03-17 19:49:39 -0700

    Positive or negative' git push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -19,7 +19,7 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	else
-	{ 
+	{
 		printf("%d is negative\n", n);
 	}
 
